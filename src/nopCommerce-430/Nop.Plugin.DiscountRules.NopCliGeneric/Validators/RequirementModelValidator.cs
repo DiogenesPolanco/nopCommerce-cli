12cc35089c7b29bf91987c34f2229d48d1bdd687
Diogenes Polanco <diogenespolancomartinez@gmail.com>
2021-09-27 14:28:02 -0400

    2.1.6 +DiscountRules 4.30

---
+++ src/nopCommerce-430/Nop.Plugin.DiscountRules.NopCliGeneric/Validators/RequirementModelValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Nop.Plugin.DiscountRules.NopCliGeneric.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugin.DiscountRules.NopCliGeneric.Validators
+{
+    /// <summary>
+    /// Represents an <see cref="RequirementModel"/> validator.
+    /// </summary>
+    public class RequirementModelValidator : BaseNopValidator<RequirementModel>
+    {
+        public RequirementModelValidator(ILocalizationService localizationService)
+        {
+            RuleFor(model => model.DiscountId)
+                .NotEmpty()
+                .WithMessage(localizationService.GetResource("Plugins.DiscountRules.NopCliGeneric.Fields.DiscountId.Required"));
+            RuleFor(model => model.NopCliGenericId)
+                .NotEmpty()
+                .WithMessage(localizationService.GetResource("Plugins.DiscountRules.NopCliGeneric.Fields.NopCliGenericId.Required"));
+        }
+    }
+}
